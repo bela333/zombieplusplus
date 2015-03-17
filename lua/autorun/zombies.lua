8ce6e5e17570190d83fe382dc63f901e3b9ad3ae
bela333 <micimackom@postafiok.hu>
2015-03-17 15:21:22 +0100

    Added healing zombies
    and fixed some English problems...

--- lua/autorun/zombies.lua
+++ lua/autorun/zombies.lua
@@ -1,32 +1,41 @@
 CreateConVar("zpp_barnicle", 1, 0, "This will enable or disable existence of transformed barnicle zombies")
 CreateConVar("zpp_headcrab", 1, 0, "This will enable or disable existence of transformed headcrab zombies")
 CreateConVar("zpp_zombie", 1, 0, "This will enable or disable existence of transformed zombies")
+CreateConVar("zpp_heal", 0, 0, "This will enable or disable the healing of zombies, on transforming an npc or player")
 
 bonemerge = false
+pills = false
 
 function GenMenu(Panel)
 	Panel:ClearControls()
 	local barnicle = vgui.Create("DCheckBoxLabel")
 	barnicle:SetPos(10,30)
 	barnicle:SetParent(Panel)
-	barnicle:SetText("Barnicle zombies")
+	barnicle:SetText("Transforming by barnicles.")
 	barnicle:SetConVar("zpp_barnicle")
 	barnicle:SetValue(0)
 	barnicle:SizeToContents()
 	local headcrab = vgui.Create("DCheckBoxLabel")
 	headcrab:SetPos(10,50)
 	headcrab:SetParent(Panel)
-	headcrab:SetText("Headcrag zombies")
+	headcrab:SetText("Transforming by headcrabs.")
 	headcrab:SetConVar("zpp_headcrab")
 	headcrab:SetValue(0)
 	headcrab:SizeToContents()
 	local zombie = vgui.Create("DCheckBoxLabel")
 	zombie:SetPos(10,70)
 	zombie:SetParent(Panel)
-	zombie:SetText("zombies")
+	zombie:SetText("Transforming by zombies.")
 	zombie:SetConVar("zpp_zombie")
 	zombie:SetValue(0)
 	zombie:SizeToContents()
+	local heal = vgui.Create("DCheckBoxLabel")
+	heal:SetPos(10,90)
+	heal:SetParent(Panel)
+	heal:SetText("Healing of zombies, on transforming an npc or player...")
+	heal:SetConVar("zpp_heal")
+	heal:SetValue(0)
+	heal:SizeToContents()
 end
 
 function CreateMenu()
@@ -41,14 +50,18 @@ function makeMeAZombie(Killer, Victim)
 	if(value["wsid"] == "170917418") then
 		bonemerge = value["mounted"]
 	end
+	if(value["wsid"] == "106427033") then
+		pills = value["mounted"]
+	end
 	
 	end)
 	
 	
 	
-	
+
 	local class = Killer:GetClass()
-	if (class=="npc_zombie" && GetConVarNumber( "zpp_zombie" ) == 1 || class=="npc_fastzombie" && GetConVarNumber( "zpp_zombie" ) == 1 || class=="npc_poisonzombie" && GetConVarNumber( "zpp_zombie" ) == 1 || class=="npc_barnacle" && GetConVarNumber( "zpp_barnicle" ) == 1 || class=="npc_headcrab" && GetConVarNumber( "zpp_headcrab" ) == 1|| class=="npc_headcrab_fast" && GetConVarNumber( "zpp_headcrab" ) == 1 || class=="npc_zombie_torso" && GetConVarNumber( "zpp_zombie" ) == 1 || class=="npc_fastzombie_torso" && GetConVarNumber( "zpp_zombie" ) == 1 ) then
+	if (class=="npc_zombie" && GetConVarNumber( "zpp_zombie" ) == 1 || class=="npc_fastzombie" && GetConVarNumber( "zpp_zombie" ) || class=="npc_poisonzombie" && GetConVarNumber( "zpp_zombie" ) == 1 || class=="npc_barnacle" && GetConVarNumber( "zpp_barnicle" ) == 1 || class=="npc_headcrab" && GetConVarNumber( "zpp_headcrab" ) == 1|| class=="npc_headcrab_fast" && GetConVarNumber( "zpp_headcrab" ) == 1 || class=="npc_zombie_torso" && GetConVarNumber( "zpp_zombie" ) == 1 || class=="npc_fastzombie_torso" && GetConVarNumber( "zpp_zombie" ) == 1 || Killer:GetViewEntity().formTable.model == "models/Zombie/fast.mdl" && GetConVarNumber( "zpp_zombie" ) == 1 || Killer:GetViewEntity().formTable.model == "models/Zombie/Classic.mdl" && GetConVarNumber( "zpp_zombie" ) == 1   || Killer:GetViewEntity().formTable.model == "models/Zombie/classic_torso.mdl" && GetConVarNumber( "zpp_zombie" ) == 1   || Killer:GetViewEntity().formTable.model == "models/zombie/fast_torso.mdl" && GetConVarNumber( "zpp_zombie" ) == 1 || Killer:GetViewEntity().formTable.model == "models/Zombie/Poison.mdl" && GetConVarNumber( "zpp_zombie" ) == 1 || Killer:GetViewEntity().formTable.model == "models/birdbrainswagtrain/zombie/fast_frame.mdl" && GetConVarNumber( "zpp_zombie" ) == 1 || Killer:GetViewEntity().formTable.model == "models/birdbrainswagtrain/zombie/classic_frame.mdl" && GetConVarNumber( "zpp_zombie" ) == 1 || Killer:GetViewEntity().formTable.model == "models/birdbrainswagtrain/zombie/poison_frame.mdl" && GetConVarNumber( "zpp_zombie" ) == 1  ) then
+		
 		if (!SERVER) then return end
 		local zombietype = ""
 		if class == "npc_zombie" then
@@ -69,8 +82,25 @@ function makeMeAZombie(Killer, Victim)
 		elseif class == "npc_zombie_torso" then
 			zombietype = "npc_zombie"
 		elseif class == "npc_fastzombie_torso" then
-		zombietype = "npc_fastzombie"
+			zombietype = "npc_fastzombie"
+		elseif Killer:GetViewEntity().formTable.model == "models/Zombie/fast.mdl" then
+			zombietype = "npc_fastzombie"
+		elseif Killer:GetViewEntity().formTable.model == "models/Zombie/Classic.mdl" then
+			zombietype = "npc_zombie"
+		elseif Killer:GetViewEntity().formTable.model == "models/zombie/fast_torso.mdl" then
+			zombietype = "npc_fastzombie"
+		elseif Killer:GetViewEntity().formTable.model == "models/Zombie/classic_torso.mdl" then
+			zombietype = "npc_zombie"
+		elseif Killer:GetViewEntity().formTable.model == "models/Zombie/Poison.mdl" then
+			zombietype = "npc_poisonzombie"
+		elseif Killer:GetViewEntity().formTable.model == "models/birdbrainswagtrain/zombie/fast_frame.mdl" then
+			zombietype = "npc_fastzombie"
+		elseif Killer:GetViewEntity().formTable.model == "models/birdbrainswagtrain/zombie/classic_frame.mdl" then
+			zombietype = "npc_zombie"
+		elseif Killer:GetViewEntity().formTable.model == "models/birdbrainswagtrain/zombie/poison_frame.mdl" then
+			zombietype = "npc_poisonzombie"
 		end
+		
 		local npc = ents.Create(zombietype) -- create the entity
 		local model = ents.Create("prop_ragdoll")
 		model:SetModel(Victim:GetModel())
@@ -81,14 +111,16 @@ function makeMeAZombie(Killer, Victim)
 		
 		npc:SetPos(Victim:GetPos()) -- position
 		npc:SetAngles(Angle(0,Victim:GetAngles().y,0))
+		if (GetConVarNumber( "zpp_heal" ) == 1)then
+		Killer:SetHealth(Killer:Health() + 10)
+		end
 		if(bonemerge) then
 		rb655_ApplyBonemerge( model, npc )
 		
 		npc:SetColor(Color(255, 255, 255, 0))
 		
 		npc:SetRenderMode( 4 )
-		npc.IsAMyZombie = true
-		npc.MyModelIs = model:GetModel()
+
 		else
 		model:Remove()
 		end
@@ -129,12 +161,11 @@ function npcZombify(victim, dmginfo)
 		else
 
 	if !table.HasValue(ZombieingNpcs, victim:GetClass()) then return end
-	if !killer:IsNPC() then return end
 	makeMeAZombie(killer, victim)
 	end
 	
 end
 
 
-hook.Add("EntityTakeDamage", "onDamage", npcZombify)
-hook.Add( "PopulateToolMenu", "Zombieplusplusmenu", CreateMenu )
+hook.Add("EntityTakeDamage", "ZombiePlusPlusZombify", npcZombify)
+hook.Add( "PopulateToolMenu", "ZombiePlusPlusMenu", CreateMenu )
