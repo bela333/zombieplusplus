6f4adf928926e7460cfc7f77241f1abc7051d36f
bela333 <micimackom@postafiok.hu>
2016-02-06 09:59:40 +0100

    Delete compile.bat

--- compile.bat
+++
@@ -1,5 +0,0 @@
-"D:\NewSteamLibrary\steamapps\common\GarrysMod\bin\gmad.exe" create -folder "D:\NewSteamLibrary\steamapps\common\GarrysMod\garrysmod\addons\Zombies" -out "pack.gma"
-"D:\NewSteamLibrary\steamapps\common\GarrysMod\bin\gmpublish.exe" update -addon "pack.gma" -id 406760913
-del pack.gma
-"C:\Program Files (x86)\Git\bin\git" push origin master
-pause
