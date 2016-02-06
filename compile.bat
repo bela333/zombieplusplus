8bc40474278fe14a3462c56d288c6ed188966b38
BuildTools <unconfigured@null.spigotmc.org>
2016-02-06 09:59:06 +0100

    Signed-off-by: BuildTools <unconfigured@null.spigotmc.org>

--- compile.bat
+++ compile.bat
@@ -1,3 +1,5 @@
-"D:\SteamLibrary\SteamApps\common\GarrysMod\bin\gmad.exe" create -folder "D:\SteamLibrary\SteamApps\common\GarrysMod\garrysmod\addons\Zombies" -out "pack.gma"
-"D:\SteamLibrary\SteamApps\common\GarrysMod\bin\gmpublish.exe" update -addon "pack.gma" -id 406760913
+"D:\NewSteamLibrary\steamapps\common\GarrysMod\bin\gmad.exe" create -folder "D:\NewSteamLibrary\steamapps\common\GarrysMod\garrysmod\addons\Zombies" -out "pack.gma"
+"D:\NewSteamLibrary\steamapps\common\GarrysMod\bin\gmpublish.exe" update -addon "pack.gma" -id 406760913
 del pack.gma
+"C:\Program Files (x86)\Git\bin\git" push origin master
+pause
--- lua/autorun/zombies.lua
+++ lua/autorun/zombies.lua
@@ -1,10 +1,109 @@
-CreateConVar("zpp_barnicle", 1, 0, "This will enable or disable existence of transformed barnicle zombies")
-CreateConVar("zpp_headcrab", 1, 0, "This will enable or disable existence of transformed headcrab zombies")
-CreateConVar("zpp_zombie", 1, 0, "This will enable or disable existence of transformed zombies")
-CreateConVar("zpp_heal", 0, 0, "This will enable or disable the healing of zombies, on transforming an npc or player")
+CreateConVar("zpp_player", 1, 128, "Players can transfrom to zombie...")
+CreateConVar("zpp_barnicle", 1, 128, "This will enable or disable existence of transformed barnicle zombies")
+CreateConVar("zpp_headcrab", 1, 128, "This will enable or disable existence of transformed headcrab zombies")
+CreateConVar("zpp_zombie", 1, 128, "This will enable or disable existence of transformed zombies")
+CreateConVar("zpp_heal", 0, 128, "This will enable or disable the healing of zombies, on transforming an npc or player")
+CreateConVar("zpp_head", 1, 128, "Spawn zombies with headcrab")
+CreateConVar("zpp_forcehead", 0, 128, "Spawn transformed zombies with headcrab too")
 
-bonemerge = false
-pills = false
+local bonemerge = false
+
+
+
+function UltimateZombieMaker(class, Killer, Victim)
+		HaveHeadcrab = 1
+		local NullVector = Vector(-0.5,-0.5,-0.5)
+		
+			if (!SERVER) then return end
+		local zombietype = ""
+		if class == "npc_zombie" then
+			zombietype = "npc_zombie"			
+			HaveHeadcrab = 0
+		elseif class == "npc_fastzombie" then
+			zombietype = "npc_fastzombie"			
+			HaveHeadcrab = 0
+		elseif class == "npc_poisonzombie" then
+			zombietype = "npc_poisonzombie"			
+			HaveHeadcrab = 0
+		elseif class == "npc_barnacle" then
+			zombietype = "npc_zombie"
+			Killer:Remove()
+		elseif class == "npc_headcrab" then
+			zombietype = "npc_zombie"
+			Killer:Remove()
+		elseif class == "npc_headcrab_fast" then
+			zombietype = "npc_fastzombie"
+			Killer:Remove()
+		elseif class == "npc_zombie_torso" then
+			zombietype = "npc_zombie"			
+			HaveHeadcrab = 0
+		elseif class == "npc_fastzombie_torso" then
+			zombietype = "npc_fastzombie"			
+			HaveHeadcrab = 0
+		elseif Killer:GetViewEntity().formTable.model == "models/Zombie/fast.mdl" then
+			zombietype = "npc_fastzombie"			
+			HaveHeadcrab = 0
+		elseif Killer:GetViewEntity().formTable.model == "models/Zombie/Classic.mdl" then
+			zombietype = "npc_zombie"			
+			HaveHeadcrab = 0
+		elseif Killer:GetViewEntity().formTable.model == "models/zombie/fast_torso.mdl" then
+			zombietype = "npc_fastzombie"			
+			HaveHeadcrab = 0
+		elseif Killer:GetViewEntity().formTable.model == "models/Zombie/classic_torso.mdl" then
+			zombietype = "npc_zombie"			
+			HaveHeadcrab = 0
+		elseif Killer:GetViewEntity().formTable.model == "models/Zombie/Poison.mdl" then
+			zombietype = "npc_poisonzombie"			
+			HaveHeadcrab = 0
+		elseif Killer:GetViewEntity().formTable.model == "models/birdbrainswagtrain/zombie/fast_frame.mdl" then
+			zombietype = "npc_fastzombie"			
+			HaveHeadcrab = 0
+		elseif Killer:GetViewEntity().formTable.model == "models/birdbrainswagtrain/zombie/classic_frame.mdl" then
+			zombietype = "npc_zombie"			
+			HaveHeadcrab = 0
+		elseif Killer:GetViewEntity().formTable.model == "models/birdbrainswagtrain/zombie/poison_frame.mdl" then
+			zombietype = "npc_poisonzombie"			
+			HaveHeadcrab = 0
+		end
+		
+		
+		local npc = ents.Create(zombietype) -- create the entity
+		local model = ents.Create("prop_ragdoll")
+		model:SetModel(Victim:GetModel())
+		npc:Spawn()
+		npc:Activate()
+		model:Spawn()
+		model:Activate()
+		
+		npc:SetPos(Victim:GetPos()) -- position
+		npc:SetAngles(Angle(0,Victim:GetAngles().y,0))
+		if (GetConVarNumber( "zpp_heal" ) == 1)then
+		Killer:SetHealth(Killer:Health() + 10)
+		end
+		if(bonemerge) then
+		
+		if(GetConVarNumber( "zpp_head" ) == 1 && HaveHeadcrab == 1 || GetConVarNumber("zpp_forcehead") == 1) then
+		bone = model:LookupBone( "ValveBiped.Bip01_Head1" )
+		Bonemerged = rb655_ApplyBonemerge( model, npc )
+		Bonemerged:ManipulateBoneScale(bone, NullVector)
+		npc:SetSubMaterial(0,"Models/effects/vol_light001")
+		else
+		Bonemerged = rb655_ApplyBonemerge( model, npc )
+		npc:SetColor(Color(0,0,0,0))
+		npc:SetRenderMode( RENDERMODE_TRANSALPHA )
+		end
+		
+		else
+		model:Remove()
+		
+		
+		
+		end
+
+		
+		if Victim:IsPlayer() then return end
+			Victim:Remove()
+end
 
 function GenMenu(Panel)
 	Panel:ClearControls()
@@ -30,12 +129,33 @@ function GenMenu(Panel)
 	zombie:SetValue(0)
 	zombie:SizeToContents()
 	local heal = vgui.Create("DCheckBoxLabel")
-	heal:SetPos(10,90)
+	heal:SetPos(10,150)
 	heal:SetParent(Panel)
 	heal:SetText("Healing of zombies, on transforming an npc or player...")
 	heal:SetConVar("zpp_heal")
 	heal:SetValue(0)
 	heal:SizeToContents()
+	local Head = vgui.Create("DCheckBoxLabel")
+	Head:SetPos(10,90)
+	Head:SetParent(Panel)
+	Head:SetText("Spawn zombies with headcrab")
+	Head:SetConVar("zpp_head")
+	Head:SetValue(0)
+	Head:SizeToContents()
+	local forcehead = vgui.Create("DCheckBoxLabel")
+	forcehead:SetPos(10,130)
+	forcehead:SetParent(Panel)
+	forcehead:SetText("Spawn transformed zombies with headcrab too")
+	forcehead:SetConVar("zpp_forcehead")
+	forcehead:SetValue(0)
+	forcehead:SizeToContents()
+	local PlayerMenu = vgui.Create("DCheckBoxLabel")
+	PlayerMenu:SetPos(10,110)
+	PlayerMenu:SetParent(Panel)
+	PlayerMenu:SetText("Players can transfrom to zombie...")
+	PlayerMenu:SetConVar("zpp_player")
+	PlayerMenu:SetValue(0)
+	PlayerMenu:SizeToContents()
 end
 
 function CreateMenu()
@@ -60,74 +180,10 @@ function makeMeAZombie(Killer, Victim)
 	
 
 	local class = Killer:GetClass()
-	if (class=="npc_zombie" && GetConVarNumber( "zpp_zombie" ) == 1 || class=="npc_fastzombie" && GetConVarNumber( "zpp_zombie" ) || class=="npc_poisonzombie" && GetConVarNumber( "zpp_zombie" ) == 1 || class=="npc_barnacle" && GetConVarNumber( "zpp_barnicle" ) == 1 || class=="npc_headcrab" && GetConVarNumber( "zpp_headcrab" ) == 1|| class=="npc_headcrab_fast" && GetConVarNumber( "zpp_headcrab" ) == 1 || class=="npc_zombie_torso" && GetConVarNumber( "zpp_zombie" ) == 1 || class=="npc_fastzombie_torso" && GetConVarNumber( "zpp_zombie" ) == 1 || Killer:GetViewEntity().formTable.model == "models/Zombie/fast.mdl" && GetConVarNumber( "zpp_zombie" ) == 1 || Killer:GetViewEntity().formTable.model == "models/Zombie/Classic.mdl" && GetConVarNumber( "zpp_zombie" ) == 1   || Killer:GetViewEntity().formTable.model == "models/Zombie/classic_torso.mdl" && GetConVarNumber( "zpp_zombie" ) == 1   || Killer:GetViewEntity().formTable.model == "models/zombie/fast_torso.mdl" && GetConVarNumber( "zpp_zombie" ) == 1 || Killer:GetViewEntity().formTable.model == "models/Zombie/Poison.mdl" && GetConVarNumber( "zpp_zombie" ) == 1 || Killer:GetViewEntity().formTable.model == "models/birdbrainswagtrain/zombie/fast_frame.mdl" && GetConVarNumber( "zpp_zombie" ) == 1 || Killer:GetViewEntity().formTable.model == "models/birdbrainswagtrain/zombie/classic_frame.mdl" && GetConVarNumber( "zpp_zombie" ) == 1 || Killer:GetViewEntity().formTable.model == "models/birdbrainswagtrain/zombie/poison_frame.mdl" && GetConVarNumber( "zpp_zombie" ) == 1  ) then
+			if (ShouldZombify(class)) then
 		
-		if (!SERVER) then return end
-		local zombietype = ""
-		if class == "npc_zombie" then
-			zombietype = "npc_zombie"
-		elseif class == "npc_fastzombie" then
-			zombietype = "npc_fastzombie"
-		elseif class == "npc_poisonzombie" then
-			zombietype = "npc_poisonzombie"
-		elseif class == "npc_barnacle" then
-			zombietype = "npc_zombie"
-			Killer:Remove()
-		elseif class == "npc_headcrab" then
-			zombietype = "npc_zombie"
-			Killer:Remove()
-		elseif class == "npc_headcrab_fast" then
-			zombietype = "npc_fastzombie"
-			Killer:Remove()
-		elseif class == "npc_zombie_torso" then
-			zombietype = "npc_zombie"
-		elseif class == "npc_fastzombie_torso" then
-			zombietype = "npc_fastzombie"
-		elseif Killer:GetViewEntity().formTable.model == "models/Zombie/fast.mdl" then
-			zombietype = "npc_fastzombie"
-		elseif Killer:GetViewEntity().formTable.model == "models/Zombie/Classic.mdl" then
-			zombietype = "npc_zombie"
-		elseif Killer:GetViewEntity().formTable.model == "models/zombie/fast_torso.mdl" then
-			zombietype = "npc_fastzombie"
-		elseif Killer:GetViewEntity().formTable.model == "models/Zombie/classic_torso.mdl" then
-			zombietype = "npc_zombie"
-		elseif Killer:GetViewEntity().formTable.model == "models/Zombie/Poison.mdl" then
-			zombietype = "npc_poisonzombie"
-		elseif Killer:GetViewEntity().formTable.model == "models/birdbrainswagtrain/zombie/fast_frame.mdl" then
-			zombietype = "npc_fastzombie"
-		elseif Killer:GetViewEntity().formTable.model == "models/birdbrainswagtrain/zombie/classic_frame.mdl" then
-			zombietype = "npc_zombie"
-		elseif Killer:GetViewEntity().formTable.model == "models/birdbrainswagtrain/zombie/poison_frame.mdl" then
-			zombietype = "npc_poisonzombie"
-		end
-		
-		local npc = ents.Create(zombietype) -- create the entity
-		local model = ents.Create("prop_ragdoll")
-		model:SetModel(Victim:GetModel())
-		npc:Spawn()
-		npc:Activate()
-		model:Spawn()
-		model:Activate()
-		
-		npc:SetPos(Victim:GetPos()) -- position
-		npc:SetAngles(Angle(0,Victim:GetAngles().y,0))
-		if (GetConVarNumber( "zpp_heal" ) == 1)then
-		Killer:SetHealth(Killer:Health() + 10)
-		end
-		if(bonemerge) then
-		rb655_ApplyBonemerge( model, npc )
-		
-		npc:SetColor(Color(255, 255, 255, 0))
-		
-		npc:SetRenderMode( 4 )
+	UltimateZombieMaker(class, Killer, Victim)
 
-		else
-		model:Remove()
-		end
-
-		
-		if Victim:IsPlayer() then return end
-			Victim:Remove()
 end
 end
 local ZombieingNpcs = {
@@ -148,13 +204,17 @@ local ZombieingNpcs = {
 
 }
 
+function ShouldZombify(class)
+	return (class=="npc_zombie" && GetConVarNumber( "zpp_zombie" ) == 1 || class=="npc_fastzombie" && GetConVarNumber( "zpp_zombie" ) == 1 || class=="npc_poisonzombie" && GetConVarNumber( "zpp_zombie" ) == 1 || class=="npc_barnacle" && GetConVarNumber( "zpp_barnicle" ) == 1 || class=="npc_headcrab" && GetConVarNumber( "zpp_headcrab" ) == 1|| class=="npc_headcrab_fast" && GetConVarNumber( "zpp_headcrab" ) == 1 || class=="npc_zombie_torso" && GetConVarNumber( "zpp_zombie" ) == 1 || class=="npc_fastzombie_torso" && GetConVarNumber( "zpp_zombie" ) == 1 );
+end
+
 
 function npcZombify(victim, dmginfo)
 
 		killer = dmginfo:GetInflictor()
 		class = killer:GetClass()
-		if(victim:GetClass() == "player") then
-			if (class=="npc_zombie" && GetConVarNumber( "zpp_zombie" ) == 1 || class=="npc_fastzombie" && GetConVarNumber( "zpp_zombie" ) == 1 || class=="npc_poisonzombie" && GetConVarNumber( "zpp_zombie" ) == 1 || class=="npc_barnacle" && GetConVarNumber( "zpp_barnicle" ) == 1 || class=="npc_headcrab" && GetConVarNumber( "zpp_headcrab" ) == 1|| class=="npc_headcrab_fast" && GetConVarNumber( "zpp_headcrab" ) == 1 || class=="npc_zombie_torso" && GetConVarNumber( "zpp_zombie" ) == 1 || class=="npc_fastzombie_torso" && GetConVarNumber( "zpp_zombie" ) == 1 ) then
+		if(victim:GetClass() == "player" && GetConVarNumber( "zpp_player" ) == 1) then
+			if (ShouldZombify(class)) then
 				makeMeAZombie(killer, victim)
 				victim:Kill()
 			end
